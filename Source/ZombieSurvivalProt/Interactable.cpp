8ab16fd3d70d5f8bf0e6855ee101b73a9b9f4864
Dungstenium <dungstenium@outlook.com>
2020-05-01 19:50:26 +0200

    Interactable has mesh from code and is a physics actor now

--- Source/ZombieSurvivalProt/Interactable.cpp
+++ Source/ZombieSurvivalProt/Interactable.cpp
@@ -2,12 +2,16 @@
 
 
 #include "Interactable.h"
+#include "Components/StaticMeshComponent.h" 
 
 // Sets default values
 AInteractable::AInteractable()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	StaticMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	RootComponent = StaticMesh;
 
 }
 
